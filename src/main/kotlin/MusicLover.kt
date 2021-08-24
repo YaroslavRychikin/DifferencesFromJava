dce08fae0e59a6c524d2d7e52bd0036dc97cb7ce
Yaroslav Rychikin <87717734+YaroslavRychikin@users.noreply.github.com>
2021-08-24 21:22:23 +0300

    Update MusicLover.kt

--- src/main/kotlin/MusicLover.kt
+++ src/main/kotlin/MusicLover.kt
@@ -2,8 +2,8 @@ fun main() {
     val amount = 10_000
     val musicLover = true
     val previousPurchaseAmount = 1111100
-    val firstDiscount = if(previousPurchaseAmount/10 <= 10000) if(previousPurchaseAmount/10 <= 1000) amount
-        else amount - 100 else amount * 0.95
+    val firstDiscount = if(previousPurchaseAmount/100 <= 10000) if(previousPurchaseAmount/100 <= 1000) amount
+        else amount - (100*100) else amount * 0.95
     val secondDiscount = if(musicLover) firstDiscount.toInt()*0.99 else firstDiscount
     val result = if(secondDiscount.toInt()%100 === 0) "${secondDiscount.toInt()/100} рублей"
         else "${secondDiscount.toInt()/100} рублей ${secondDiscount.toInt()%100} копеек"
